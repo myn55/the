752b5aa1d38b8253b9ba119e172ccdb1d09c2b0d
myn55 <56205864+myn55@users.noreply.github.com>
2020-11-20 19:38:22 -0500

    Create Keybinds.lua

---
+++ StarterPlayer/Keybinds.lua
@@ -0,0 +1,15 @@
+-- Author: Coronhaus
+local keybinds = {
+	C = 'PlayerRoll';
+	I = 'PlayerInventory';
+	X = 'Prone';
+	LeftShift = 'PlayerSprint';
+	LeftControl = 'Crouch';
+	Period = 'EmoteMenu';
+	
+	SprintToggle = false;
+	CrouchToggle = false;
+	ProneToggle = false;
+}
+
+return keybinds
